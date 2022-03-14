882395b2913d97ab458bc6db25144dad5f107429
joohee_noah <joohee195@gmail.com>
2022-03-14 10:20:57 +0900

    설명추가/ 과제풀이

--- Admin220311.sql
+++ Admin220311.sql
@@ -23,10 +23,20 @@ GRANT update on scott.emp TO usertest01;
 
 -----------  과제    -----------------
 --1. usertest02 생성
+drop user usertest02;
 create user usertest02 IDENTIFIED BY tiger;
 grant create session to usertest02;
+
 --2. 권한을 주어가며  scott의 dept Table권한을 주며 수행( 한 번은 되고 , 한번은 안 되게)
 grant create table, create view to usertest02;
+--실제로는 권한을 이렇게 줌. ★★★★★ 현장에서 DBA가 개발자 권한 부여, Resource -> RBAC의 Role임. 
 grant connect, resource to usertest02;
-select * from scott.dept;
+
 --3. Insert / Update /Select /Delete
+grant insert on scott.dept to usertest02;
+
+grant update on scott.dept to usertest02;
+
+grant select on scott.dept to usertest02;
+
+grant delete on scott.dept to usertest02;
