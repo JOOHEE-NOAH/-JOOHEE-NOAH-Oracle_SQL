352306b2490678178f099e3c98ea4217fc0b9f4e
joohee_noah <joohee195@gmail.com>
2022-03-14 10:18:57 +0900

    과제풀이

--- usertest02_220311.sql
+++ usertest02_220311.sql
@@ -1,1 +1,14 @@
 select * from scott.dept;
+
+INSERT INTO SCOTT.dept(DEPTNO,DNAME,LOC)
+    VALUES(50,'KIM','SEOUL');
+
+update scott.dept
+set dname = 'KIM'
+where deptno = 50;
+
+DELETE
+from scott.dept
+where deptno=50;
+
+commit; --> 커밋해야 실제 데이터에서 수정되어 보여진다.
