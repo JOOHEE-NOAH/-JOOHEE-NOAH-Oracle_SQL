4b205fe4605ef45c0a54ab373f72436660ca40e1
JOOHEE-NOAH <joohee195@gmail.com>
2022-03-15 09:48:53 +0900

    22/03/15 설명 추가

--- scott220314.sql
+++ scott220314.sql
@@ -36,51 +36,55 @@ insert into dept values(50, 'OPERATIONS', 'BOSTON');
 INSERT INTO EMP VALUES
 (1010,'FK Test1','CLERK', 7902, TO_DATE('17-12-1980','DD-MM-YYYY'),800,NULL,50);
 
--- 1) 기본--> restrict : 자식 존재 삭제 안됨.
+--테이블 우 마우스 -> 편집 -> 제약조건 -> 외래키 선택-> 삭제 시 선택
+-- 1) 기본(작업없음)--> restrict : 자식 존재 삭제 안됨.
 DELETE dept
 where deptno = 50;
 
--- 2) SET NULL로 설정 --> 부모쪽 행은 사라지고 자식의 외래키는 null이 됨
+-- 2) SET NULL(널로 설정)로 설정 --> 부모쪽 행은 사라지고 자식의 외래키는 null이 됨
 DELETE dept
 where deptno = 50;
 
--- 3) Cascading Delete : 같이 죽자
+-- 3) Cascading Delete(종속삭제) : 같이 죽자
 DELETE dept
 where deptno = 50;
 
 --------------------------------------------
 -------  ★★   PL/SQL  ★★--------------
 --------------------------------------------
---- 1. 함수(Function)
+--- 1. 함수(Function): return 있음
 --- 특정한 수에 세금을 7%로 계산하는 Function을 작성하면
 create or replace FUNCTION tax
-    (p_num in number) -- Parameter
-RETURN number         -- Return Value
+    (p_num in number) -- Parameter (없으면 안써도 됨)
+RETURN number         -- Return Value★★이게 있어야 함수
 Is
-    v_tax number;     -- 변수 선언
+    v_tax number;     -- 변수 선언(반드시 RETURN 형식과 일치해야한다. 여기선 number)
 BEGIN                 --함수 시작
 --세금 7%
     v_tax := p_num * 0.07;
-    RETURN(v_tax);
-END;
+    RETURN(v_tax);      
+END; -->실행시키면 함수에 들어감
 
---is 와 begin 사이에 변수 선언을 해준다. 변수라는 의미로 v_로 많이 작성한다.
+--is 와 begin 사이에 변수 선언을 해준다.(변수가 있다면) 변수라는 의미로 v_로 많이 작성한다.
 -- begin과 end 사이에 로직을 구성한다. =이 아니라 :=임을 명심!
+--is, begin, end에는 세미콜론
 
 --2.함수호출
 select tax(200) from dual;
 
 select profno , name, sal, tax(sal)
 from professor;
-
+--또는 함수 -> Tax 우클릭 -> 실행 -> 매개변수에 값입력으로 실행시켜도 됨
 
 --------------------------------------------------------------
--- Procedure
+-- Procedure : 나중에 값을 받아오려면 out을 사용해야함(리턴 값을 지정하지 않는다.)
 -- 1. 파라메타 : (p_deptno , p_dname ,   p_loc)
 -- 2. dept TBL에 Insert Procedure
 --------------------------------------------------------------
 CREATE OR REPLACE PROCEDURE Dept_Insert
-    (p_deptno in dept.deptno%type, --dept.deptno%type: dept테이블에서 deptno의 data_type이 어떻든 똑같이 해줘라. 라는 뜻
+    (p_deptno in dept.deptno%type, --dept.deptno%type: dept테이블에서 deptno의 data_type이 어떻든 똑같이 해줘라. 라는 뜻.
+                                   --타입 같게 하는 이유: 레이아웃을 바꾸는 순간 해당 프로시저를 참조한 모든것에서 오류가 발생한다.
+                                   --이때 이런식으로 선언하면 어떻게 바뀌든 유동적으로 자동으로 변하기 때문에 일일히 변경해줄 필요가 없어진다.
      p_dname in dept.dname%type,
      p_loc in dept.loc%type)
 IS
@@ -88,7 +92,8 @@ BEGIN
     INSERT into dept VALUES(p_deptno, p_dname, p_loc);
     COMMIT;
 END;
-    
+--> 프로시저에서 확인
+
 
 
 
